cb61837a3f6c706490d2529fb4281995a7bd5341
graeme benson <62527977+g-r-a-e-m-e@users.noreply.github.com>
2024-03-21 20:10:14 +0000

    Upload pictures for site, update About.jsx and Home.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,33 +10,31 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/max-mckinnon-c9OCWLka764-unsplash";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "An image of the Milky Way galaxy.";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a data scientist and am studying Astrophysics at Old Dominion University. Thank you for visiting my site.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Machine learning",
+  "Statistical analysis",
+  "Python programming",
+  "R programming",
 ];
 
 /**
@@ -45,7 +43,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I aim to use data to solve problems and make the world a better place for all. I am working to earn a degree in Astrophysics and hope to one day work as a research scientist in this field.";
 
 const About = () => {
   return (
@@ -60,7 +58,7 @@ const About = () => {
           textAlign: "center",
         }}
       >
-        <h2>About Myself</h2>
+        <h2>About Me</h2>
         <p className="large">{description}</p>
         <hr />
         <ul
